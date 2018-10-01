2599884091a43036cf8b3dfc2197ad9c439d25c7
RiskaSintiaDewi <42725266+RiskaSintiaDewi@users.noreply.github.com>
2018-09-30 20:10:53 -0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,23 @@
-<?php
+<?php  
 
+$nim=$_POST['nim'];
+$nama=$_POST['nama'];
+$email=$_POST['email'];
+$komentar=$_POST['komentar'];
 
+echo "NIM : $nim<br>";
+echo "Nama : $nama<br>";
+echo "Email : $email<br><br>";
+echo "Komentar : $komentar<br><br>";
 
+$string = "$komentar";
+$hasil = str_word_count($string);
+echo "Jumlah : $hasil kata<br>";
 
+if ($hasil < 5) {
+	echo "<br>Maaf, kata yang Anda inputkan kurang.";
+}else{
+	die (" ");
+}
 
 ?>
