c8c9b00f158a8e3c57c81adfe2314c024e23486b
RiskaSintiaDewi <42725266+RiskaSintiaDewi@users.noreply.github.com>
2018-09-30 20:07:19 -0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,35 @@
-<?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table align="center">
+		<form action="prosesregis.php" method="POST">
+			<tr>
+				<td>NIM</td> 
+				<td>:</td>
+				<td> <input type="number" maxlength="10" name="nim"></td>
+			</tr>
+			<tr>
+				<td>Nama</td> 
+				<td>:</td>
+				<td> <input type="text" maxlength="25" name="nama"></td>
+			</tr>
+			<tr>
+				<td>Email</td> 
+				<td>:</td>
+				<td> <input type="email" name="email"></td>
+			</tr>
+			<tr>
+				<td>Komentar</td> 
+				<td>:</td>
+				<td> <input type="text" name="komentar"></td>
+			</tr>
+			<tr>
+				<td colspan="3" align="center"><br><input type="submit" name="Kirim" value="Kirim"></td>
+			</tr>
+		</form>
+	</table>
+</body>
+</html>
